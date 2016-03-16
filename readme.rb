c0a613bae92edd52f758bd5401eba56bc023204a
Keith Loughnane <keith.loughnane@gmail.com>
2016-03-16 13:47:12 +0000

    Create readme.rb

---
+++ readme.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  def current_user
+  
+    I like to keep a record of the projects I work on even if I can't post the code online.
+    This project is a placeholder for a ruby on rails system I developed at Impala-Technology
+    for managing parking tickets. It is a SAAS version of the current Progress software developed
+    for the desktop.
+    
+    If you are interested in seeing the system or need parking systems contact kloughnane@impala-technoloy.com
+    or akeating@impala-technology.com
+  
+  end
+end
